60225be8491bfaf53bf99940ab138f0649dbf3a1
david.corrado <david.corrado@well.co>
2022-04-20 12:44:53 -0500

    initial project

---
+++ settings.gradle.kts
@@ -0,0 +1,11 @@
+pluginManagement {
+    repositories {
+        google()
+        gradlePluginPortal()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "KMPWithWeb"
+include(":androidApp")
+include(":shared")
---
+++ shared/src/commonMain/kotlin/com/example/kmpwithweb/Platform.kt
@@ -0,0 +1,5 @@
+package com.example.kmpwithweb
+
+expect class Platform() {
+    val platform: String
+}
